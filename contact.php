43b812e35ca206830fd2411de22f783c63f65975
Srikrishna0 <41792042+Srikrishna0@users.noreply.github.com>
2020-04-11 14:41:34 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Contact Us</title>
+        
+        <link rel="stylesheet" href="newcss.css">
+        <style>
+            .heading{
+    font-weight:bold;
+    color:#2E4372;
+}
+        </style>
+       
+    </head>
+        <?php include 'header.php' ?>
+        <div class='content_customer'>
+            <h3 style="text-align:center;color:#2E4372;"><u>Contact Us</u></h3>
+            
+            <div class="contact">
+            <h3 style="color:#2E4372;"><u>Guntur Branch</u></h3>
+            <p><span class="heading">Address - </span>OLD GUNTUR,Guntur,AndhraPradesh</p>
+            <p><span class="heading">Tel - </span>033-456892/12</p>
+            <p><span class="heading">Email - </span>gunturbranch@onlinebank.com</p>
+            </div>
+        </div>
+        </html>
